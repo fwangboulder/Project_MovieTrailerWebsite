a4bd5cca2c773e1c02ae5436c666825d5633ed69
Fang Wang <fwangboulder@gmail.com>
2017-01-02 22:02:10 -0800

    commit

--- media.py
+++ media.py
@@ -1,10 +1,20 @@
+##########################################
+# Project 3: Movie Website
+# Date Started: 10/31/2016
+# Date Completed: 10/31/2016
+# Submitted by: Jon Wayland
+##########################################
+
+######################################## Media File ####################################################
+# Description: This file creates the class Movie to allow for instances of this class to be used in the
+#              entertainment.py file. This definition of the class Movie was obtained through the course
+########################################################################################################
+
 #!/usr/bin/env python
 import webbrowser
 
 
 class Movie():
-    """This is a Movie class with title, story line, poster image and
-    youtube trailer url. It contains one method to show trailer."""
 
     VALID_RATINGS = ['G', 'PG', 'PG-13', 'R']
 
